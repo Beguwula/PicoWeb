31cb0d50134ae07c7e92f97241ebcfa6b3a62644
Beguwula <171718612+Beguwula@users.noreply.github.com>
2024-08-04 15:10:28 +0300

    Update browser.py

--- browser.py
+++ browser.py
@@ -4,7 +4,13 @@ from time import sleep
 import machine
 import os
 import urequests
-def connect(ssid):
+def connect(ssid, password):
+    use_pass = input("Use password for connecting [Y/n]? ")
+    if use_pass == "Y" or use_pass == "n":
+        connect_pass(ssid, password)
+    elif use_pass == "N" or use_pass == "n":
+        connect_no_pass(ssid)
+def connect_no_pass(ssid):
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     wlan.connect(ssid)
@@ -12,7 +18,15 @@ def connect(ssid):
         print('Waiting for connection...')
         sleep(1)
     print(wlan.ifconfig())
-def download(url):
+def connect_pass(ssid, password):
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(ssid, password)
+    while wlan.isconnected() == False:
+        print('Waiting for connection...')
+        sleep(1)
+    print(wlan.ifconfig())
+def load_page(url):
     response = urequests.get(url)
     if response.status_code == 200:
         print(response.text)
